d9d68a8918195216b99ad8c26fe063fa03624f08
mysticdrew <drewhaas@gmail.com>
2019-11-30 14:33:24 -0600

    Create TrimpAutoStanceTM.js

---
+++ TrimpAutoStanceTM.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Trimps Auto Stance
+// @namespace    Trimps
+// @version      0.1
+// @description  Auto Stance D for maps, S for world and Void
+// @author       Mysticdrew
+// @include        *trimps.github.io*
+// @include        *kongregate.com/games/GreenSatellite/trimps
+// @grant        none
+// ==/UserScript==
+var script = document.createElement('script');
+script.id = 'TrimpsAutoStance-script';
+script.src = 'https://mysticdrew.github.io/TrimpsAutoStance/TrimpsAutoStance.js';
+document.head.appendChild(script);
