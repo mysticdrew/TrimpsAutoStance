b03ace795ebfc74cdaff3693a6fb8864d7ca0ec6
mysticdrew <drewhaas@gmail.com>
2019-11-30 12:53:31 -0600

    location detection

--- TrimpsAutoStance.js
+++ TrimpsAutoStance.js
@@ -1,14 +1,35 @@
 (function run() {
     let dom = document.getElementById('formation2');
     let scry = document.getElementById('formation4');
-
     if (dom !== null && scry !== null) {
-        setNewFormation(2, dom);
+        if (inBionicMap()) {
+            console.log('in bionic')
+            // setNewFormation(2, dom);
+        } else if (inVoidMap()) {
+            console.log('in void')
+            // setNewFormation(4, scry);
+        } else if (inWorld()) {
+            console.log('in world')
+            // setNewFormation(4, scry);
+        }
     }
-
-
     setTimeout(run, 1000)
 })();
+
+function inVoidMap() {
+    let mapButtons = document.getElementById('repeatVoidsContainer');
+    return mapButtons.style.cssText === 'display: block;';
+}
+
+function inBionicMap() {
+    let mapButtons = document.getElementById('climbBwContainer');
+    return mapButtons.style.cssText === 'display: block;';
+}
+
+function inWorld() {
+    let mapButtons = document.getElementById('extraMapBtns');
+    return mapButtons.classList.contains('col-xs-off');
+}
 
 function setNewFormation(formation, button) {
     if (button.classList.contains('formationStateDisabled')) {
