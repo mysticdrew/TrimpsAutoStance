dbd798c0b1fe179fc53fd32aa7ae18b6af2de30a
mysticdrew <drewhaas@gmail.com>
2019-11-30 11:55:27 -0600

    Start

--- TrimpsAutoStance.js
+++ TrimpsAutoStance.js
@@ -1,1 +1,17 @@
+(function () {
+    'use strict';
 
+    (function run() {
+        let dom = document.getElementById('formation2');
+        // var scry = $('#formation4');
+        console.log("is dom null");
+        if (dom !== null) {
+            console.log("dom not null");
+            console.log(dom.className);
+        }
+
+        //   console.log(scry.attr('class'));
+        setTimeout(run, 1000)
+    })()
+
+})();
